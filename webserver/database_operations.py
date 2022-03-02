b33c2f0cc290a274acd842f6f214a50aeb8fb1d1
bnicasio <b.nicasio@gbh.com.do>
2022-03-01 22:04:39 -0400

    Fix typo on linting

--- webserver/database_operations.py
+++ webserver/database_operations.py
@@ -11,12 +11,13 @@ app.config['MYSQL_DATABASE_PASSWORD'] = config('DB_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = config('DB_DATABASE')
 mysql.init_app(app)
 
+
 def insert(value):
     sql = f"insert into names (name) values('{value}');"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
-    last_id = str(cursor.lastrowid) # <- Get last inserted name ID
+    last_id = str(cursor.lastrowid)     # <- Get last inserted name ID
     print(last_id)
     conn.commit()
     print("sending value" + last_id)
@@ -28,20 +29,21 @@ def select_value(value):
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
-    names = cursor.fetchone() # <- Store name value
+    names = cursor.fetchone()   # <- Store name value
     print("Printing the select:")
     print(names)
     conn.commit()
     return names
+
 
 def count_rows():
     sql = 'select COUNT(*) from names;'
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
-    rows = cursor.fetchone() # <- Store row count
+    rows = cursor.fetchone()    # <- Store row count
     sql = 'select * from names ORDER BY id DESC LIMIT 10;'
     cursor.execute(sql)
-    data = cursor.fetchall() # <- Store all names
+    data = cursor.fetchall()    # <- Store all names
     conn.commit()
     return (rows, data)
